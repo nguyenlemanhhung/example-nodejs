7e8909496a8ecc5e52092504fd8628e9f17ee8e9
nguyenlemanhhung <github.nohssiw1905@gmail.com>
2023-04-21 12:04:24 +0700

    config server and check API

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import { Express, Request, Response } from "express";
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Api is running...");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
